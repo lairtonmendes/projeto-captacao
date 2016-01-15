d6d62194dc585c2bf1e78d54b265275c44195a03
lairtonmendes <lairton.mendes@gmail.com>
2016-01-15 08:13:40 -0300

    Modificações na HOME

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 	def index
-		
+		@coordenadas = Coordenada.all
+    	@hash = Gmaps4rails.build_markers(@coordenadas) do |coordenadas, marker|
+      		marker.lat coordenadas.lat
+      		marker.lng coordenadas.lon
+      		marker.infowindow "<b>#{coordenadas.ot} </b><br> lat: #{coordenadas.lat} , lng: #{coordenadas.lon}"
+      	end
 	end
 end
